4c8a71d40448fdfbf107a88163aefdf11dd8d491
Alex <ahansen2@trinity.edu>
2015-12-27 22:55:24 -0600

    now makes the sections according to song form.

--- musician.scala
+++ musician.scala
@@ -12,19 +12,48 @@ val cutOff               = 8
 val featureProbability   = 1//out of 100
 val songForm             = "AABA"
 val melodyVariation      = .01 // chance that the melody wil vary when the part repeats
-val sectionLength        = 2
+val sectionLength        = 1
+val chords               = Array("Gmaj", "Cmaj", "Bmin")
 //var potentialNotes       = Array("a", "b")
 
 val test2 = getMelody(10)
 val player = new Player
 //player.play(test2)
-val toPlay = makeSection
+val toPlay = getsections
 val string = "V0 " + toPlay._1 + " V2 " + toPlay._2 
+
 player.play(string)
 
 
 
 // Makesong function that will make a few melodies, attach them to different parts of the song, and repeat things.
+
+def getsections():(String, String) = {
+  val numberOfSections = songForm.distinct.length
+  var toReturn:Array[(String, String)] =  Array()
+  for (i <- 0 until numberOfSections) {
+    toReturn = toReturn :+ makeSection
+  }
+  var forMeasuring:Array[((String, String), Char)] = Array()
+  for (i <- 0 until numberOfSections) {
+    forMeasuring = forMeasuring :+ (toReturn(i), songForm.distinct(i))
+  }
+  var toReturn2:Array[(String, String)] = Array()
+  for (i <- 0 until songForm.length) {
+    for (j <- 0 until forMeasuring.length) {
+      if (forMeasuring(j)._2 == songForm(i))
+        toReturn2 = toReturn2 :+ forMeasuring(j)._1
+    }
+  }
+  var toReturn3:(String, String) = ("", "")
+  for (i <- 0 until toReturn2.length) {
+    toReturn3 = ((toReturn3._1 + toReturn2(i)._1),(toReturn3._2 + toReturn2(i)._2)) 
+  }
+//  println(toReturn3._1 + toReturn3._2)
+  toReturn3
+}
+
+
 
 // Function that makes one section
 def makeSection():(String, String) = {
@@ -32,6 +61,10 @@ def makeSection():(String, String) = {
   val harmony = getHarmony(sectionLength)
   (melody, harmony)
 
+}
+
+def randomChord():String = {
+  chords(Random.nextInt(chords.length))
 }
 
 def getHarmony(numOfMeasures:Int):String = {
@@ -42,7 +75,7 @@ def getHarmony(numOfMeasures:Int):String = {
   }
   for (i <- 0 until rhythms.length) {
     for (j <- 0 until rhythms(i).length) {
-      rhythms(i)(j) = "Gmaj" + rhythms(i)(j)
+      rhythms(i)(j) = randomChord + rhythms(i)(j)
     }
   }
   var toReturnString = ""
@@ -52,7 +85,7 @@ def getHarmony(numOfMeasures:Int):String = {
     }
     toReturnString = toReturnString + " |"
   }
-  println(toReturnString)
+//  println(toReturnString)
   toReturnString
 }
 
